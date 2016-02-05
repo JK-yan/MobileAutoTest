a0e60892910ccefe707cebbc7a1389d0e219f2ae
jackiezero <461831455@qq.com>
2016-02-05 10:48:24 +0800

    更新了一下截图功能，因公司电脑上maven有点问题，等待自己电脑调试

--- src/com/mobile/pages/LoginPage.java
+++ src/com/mobile/pages/LoginPage.java
@@ -3,11 +3,11 @@ package com.mobile.pages;
 import org.openqa.selenium.By;
 
 public class LoginPage {
-	//获取登录界面
-public final static By LOGIN_BUTTON=By.name("登录或注册");
-//获取账户密码输入框
-public final static By USER_ACCOUNT=By.name("邮箱或手机号");
+	//鑾峰彇鐧诲綍鐣岄潰
+public final static By LOGIN_BUTTON=By.name("登录");
+//鑾峰彇璐︽埛瀵嗙爜杈撳叆妗�
+public final static By USER_ACCOUNT=By.name("閭鎴栨墜鏈哄彿");
 public final static By USER_PASSWORD=By.xpath("//android.widget.EditText[@resource-id='com.zhihu.android:id/password']");
-public final static By CREAT_ACCOUNT=By.xpath("//com.zhihu.android:id/btn_register[@text='注册']");
-public final static By LOGIN=By.xpath("//android.widget.Button[@text='登录']");
+public final static By CREAT_ACCOUNT=By.xpath("//com.zhihu.android:id/btn_register[@text='娉ㄥ唽']");
+public final static By LOGIN=By.xpath("//android.widget.Button[@text='鐧诲綍']");
 }
--- src/com/mobile/testcase/login/TestCaseLogin.java
+++ src/com/mobile/testcase/login/TestCaseLogin.java
@@ -1,6 +1,9 @@
 package com.mobile.testcase.login;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import com.mobile.app.listener.Screenshort;
 
 import io.appium.java_client.android.AndroidDriver;
 
@@ -13,9 +16,10 @@ public class TestCaseLogin {
 			Thread.sleep(3000);
 		} catch (Exception e) {
 			// TODO: handle exception
+			Screenshort.Screenshort();
 			System.out.println("登录失败");
 			e.printStackTrace();
-		
+			
 		}
 		
 	}
